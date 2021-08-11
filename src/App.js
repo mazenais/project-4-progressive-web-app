2c065e60b8d252d44e2b849c228e142dde7c076f
mazenais <mazen.aboismail@gmail.com>
2021-08-11 17:03:22 +0200

    fectching the main data from the API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import Header from './components/ui/Header'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import "./App.css";
 
 const App = () => {
-  
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.thesportsdb.com/api/v1/json/1/all_sports.php`
+      );
+
+      console.log(result.data);
+    };
+    fetchItems()
+  }, []);
+
   return (
     <div className="container">
       <Header />
     </div>
   );
-}
+};
 
 export default App;
