6255fe2d557ee107e729d543aae6d2f78e6d9dd1
mazenais <mazen.aboismail@gmail.com>
2021-08-11 16:05:50 +0200

    create react app

---
+++ src/components/sports/SportsGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SportsGrid = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SportsGrid
---
+++ src/components/sports/SportsItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SportsItem = ({item}) => {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SportsItem
