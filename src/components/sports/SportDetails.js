f4b396eeeb10e8067a86576781b751a81ac7a8a7
mazenais <mazen.aboismail@gmail.com>
2021-08-19 17:34:13 +0200

    add login page and return button

--- src/components/sports/SportDetails.js
+++ src/components/sports/SportDetails.js
@@ -1,5 +1,6 @@
-import React  from "react";
+import React, { useEffect, useState }  from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios'
 import {BrowserRouter as Router, useHistory} from "react-router-dom"
 
 const SportDetails = ({items}) => {
@@ -11,16 +12,31 @@ const handleHistory = () => {
 
 }
 
-  let { name } = useParams();
-  console.log("name :>>", name);
+  let { idTeam } = useParams();
+  console.log("idTeam :>>", idTeam);
   
+  const [team, setTeam] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`
+      );
+
+      console.log(result.data);
+      setTeam(result.data.teams[0])
+      setIsLoading(false)
+    };
+    fetchItems()
+  }, []);
  
 
   return (
     <div>
       <button onClick={handleHistory}>back</button>
       <h2>Details</h2>
-      <p>{name}</p>
+      <p>{team?.strDescriptionEN}</p>
      
     </div>
     
