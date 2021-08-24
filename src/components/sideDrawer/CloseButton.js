2e058f1d4256f624d03ec67349a81b26d6afd204
mazenais <mazen.aboismail@gmail.com>
2021-08-24 18:47:23 +0200

    editing the side drawer and adding a close button

---
+++ src/components/sideDrawer/CloseButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CloseButton = props => {
+
+    return (
+        <button onClick={props.click}>
+             <div className="button_line" />
+        <div className="button_line" />
+        <div className="button_line" />
+            close
+        </button>
+    )
+}
+
+export default CloseButton
